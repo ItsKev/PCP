41b4eb1a20cda11d55d08f9fd2f3cd7ba09c5602
Kevin Huber <kevin.huber@hotmail.com>
2018-09-28 11:30:32 +0200

    SW02 Ex01,03,04 done.

---
+++ Prolog/SW02/ex01.pl
@@ -0,0 +1,34 @@
+female(mary). female(liz). female(mia). female(tina). female(ann). female(sue).% all females
+male(mike). male(jack). male(fred). male(tom). male(joe). male(jim). % all males
+parent(mary, mia). parent(mary, fred). parent(mary, tina). % all childern of mary
+parent(mike, mia). parent(mike, fred). parent(mike, tina). % all children of mike
+parent(liz, tom). parent(liz, joe). % allchildern of liz
+parent(jack, tom). parent(jack, joe). % all childern of jack
+parent(mia, ann). % all childern of mia
+parent(tina, sue). parent(tina, jim). % all childern of tina
+parent(tom, sue). parent(tom, jim). % all childern of tom
+
+% a)
+mother(X, Y) :-
+    parent(X, Y),
+    female(X).
+
+father(X, Y) :-
+    parent(X, Y),
+    male(X).
+
+% b)
+sibling(X, Y) :-
+    mother(Z, X),
+    mother(Z, Y).
+
+% c)
+grandmother(X, Y) :-
+    mother(X, Z),
+    parent(Z, Y).
+
+% d)
+offspring(X, Y) :- parent(Y, X).
+offspring(X, Y) :-
+    parent(Y, Z),
+    offspring(X, Z).
---
+++ Prolog/SW02/ex04.pl
@@ -0,0 +1,15 @@
+n(red, yellow).
+n(red, green).
+n(green, yellow).
+n(green, red).
+n(yellow, red).
+n(yellow, green).
+
+color(LU,NW,OW,SZ,UR,ZG) :-
+    UR = yellow,
+    SZ = red,
+    n(LU, ZG), n(LU, SZ), n(LU, NW), n(LU, OW),
+    n(ZG, LU), n(ZG, SZ),
+    n(SZ, UR), n(SZ, NW),
+    n(UR, NW),
+    n(NW, OW).
